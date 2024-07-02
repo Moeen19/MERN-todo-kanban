907573cf556f3cdeb84e2eb14756b7b513a0526f
moeen <moeen12yt@gmail.com>
2024-07-02 20:33:10 +0500

    kekw

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: '/'
     })
