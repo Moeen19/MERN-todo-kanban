1e6dc6b1bd3283832135563e89a0efde72d047e9
moeen <moeen12yt@gmail.com>
2024-07-02 21:49:55 +0500

    fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: true,
-        SameSite: 'none',
+        same_site: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: '/'
     })
