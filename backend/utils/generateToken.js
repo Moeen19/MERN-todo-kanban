9dc24b5a5fd32aafc1295d992db1b4995579d834
moeen <moeen12yt@gmail.com>
2024-07-03 19:51:51 +0500

    kekw

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,11 +4,24 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '30d'
     })
+    let domain;
+    if (process.env.NODE_ENV === 'development') {
+        // Production environment (Vercel frontend and Railway backend)
+        if (process.env.NEXT_PUBLIC_VERCEL_URL.includes('vercel.app')) {
+            domain = '.vercel.app';
+        } else if (process.env.NEXT_PUBLIC_VERCEL_URL.includes('up.railway.app')) {
+            domain = '.up.railway.app';
+        }
+    } else {
+        // Local development environment
+        domain = 'localhost';
+    }
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: true,
         sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000,
+        domain: domain,
         path: '/'
     })
 }
