bb2af1e99f9aee8e915786faef58546a62a7d694
moeen <moeen12yt@gmail.com>
2024-07-04 15:50:48 +0500

    kekw

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,49 @@
 import express from "express";
-import connectDB from "./db/connect.js"
-import todoRoutes from "./routes/todoRoutes.js"
+import connectDB from "./db/connect.js";
+import todoRoutes from "./routes/todoRoutes.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
 import notFound from "./middleware/notFound.js";
-import userRoutes from "./routes/userRoutes.js"
+import session from "express-session";
+import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
 connectDB();
-app.use(cors({
-  origin: 'https://mern-todo-kanban-vraf.vercel.app',
-  credentials: true,
-  methods: ['DELETE', 'PUT', 'GET', 'POST'],
-}))
+app.use(
+  cors({
+    origin: "https://mern-todo-kanban-vraf.vercel.app",
+    credentials: true,
+    methods: ["DELETE", "PUT", "GET", "POST"],
+  })
+);
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
 // logger middleware
 app.use(logger);
 
 // cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
-app.use("/users", userRoutes)
-app.use("/todos", todoRoutes)
+app.use("/users", userRoutes);
+app.use("/todos", todoRoutes);
 
 // Error handling middleware
-app.use(notFound)
+app.use(notFound);
 app.use(errorHandler);
 
 // Listen
