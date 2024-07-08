ab369ef651d2ff98df835576d9d1c9a8d9a45c1a
moeen <moeen12yt@gmail.com>
2024-07-08 16:56:40 +0500

    kekw

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -1,8 +1,8 @@
-// "use client"
+"use client"
 import Link from "next/link";
 import RegisterUser from "@/components/RegisterUser";
-import { cookies } from "next/headers";
-import { redirect } from "next/dist/server/api-utils";
+// import { cookies } from "next/headers";
+// import { redirect } from "next/dist/server/api-utils";
 import { useEffect } from "react";
 
 export default function signup() {
