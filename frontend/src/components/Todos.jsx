931c211bf441a4080b4157357a41dc075ff086a1
moeen <moeen12yt@gmail.com>
2024-07-08 18:23:11 +0500

    kek

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Todos({ todos }) {
-  console.log(todos)
+  console.log(todos, 'parent')
   const router = useRouter();
   const [notDoneTodos, setNotDoneTodos] = useState([]);
   const [Upd, setUpd] = useState(false);
